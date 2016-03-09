bedcb0a866617deaffdea236a6db133385429ead
Philip Herron <herron.philip@googlemail.com>
2016-03-09 15:13:24 +0000

    More rtsp response tests

--- test/RtspResponseTests.cc
+++ test/RtspResponseTests.cc
@@ -6,7 +6,9 @@
 
 TEST(RTSP_RESPONSE, TestThatCanParseStatusCode) {
     const char *test_response =
-        "RTSP/1.0 200 OK\r\nCSeq: 0\r\nContent-Type: application/sdp\r\n";
+        "RTSP/1.0 200 OK\r\n"
+        "CSeq: 0\r\n"
+        "Content-Type: application/sdp\r\n";
 
     Surge::Response resp((unsigned char *)test_response, strlen(test_response));
     Surge::RtspResponse response(&resp);
@@ -16,7 +18,9 @@ TEST(RTSP_RESPONSE, TestThatCanParseStatusCode) {
 
 TEST(RTSP_RESPONSE, TestThat200StatusCodeIsOk) {
     const char *test_response =
-        "RTSP/1.0 200 OK\r\nCSeq: 0\r\nContent-Type: application/sdp\r\n";
+        "RTSP/1.0 200 OK\r\n"
+        "CSeq: 0\r\n"
+        "Content-Type: application/sdp\r\n";
 
     Surge::Response resp((unsigned char *)test_response, strlen(test_response));
     Surge::RtspResponse response(&resp);
@@ -26,7 +30,9 @@ TEST(RTSP_RESPONSE, TestThat200StatusCodeIsOk) {
 
 TEST(RTSP_RESPONSE, TestThatNon200StatusCodeIsNotOk) {
     const char *test_response =
-        "RTSP/1.0 500 OK\r\nCSeq: 0\r\nContent-Type: application/sdp\r\n";
+        "RTSP/1.0 500 OK\r\n"
+        "CSeq: 0\r\n"
+        "Content-Type: application/sdp\r\n";
 
     Surge::Response resp((unsigned char *)test_response, strlen(test_response));
     Surge::RtspResponse response(&resp);
@@ -36,10 +42,65 @@ TEST(RTSP_RESPONSE, TestThatNon200StatusCodeIsNotOk) {
 
 TEST(RTSP_RESPONSE, TestThatCanParseBody) {
     const char *test_response =
-        "RTSP/1.0 200 OK\r\nCSeq: 0\r\nContent-Type: application/sdp\r\n\r\nBODY\r\nBODY\r\n\r\n";
+        "RTSP/1.0 200 OK\r\n"
+        "CSeq: 0\r\n"
+        "Content-Type: application/sdp\r\n"
+        "\r\n"
+        "BODY\r\n"
+        "BODY\r\n"
+        "\r\n";
 
     Surge::Response resp((unsigned char *)test_response, strlen(test_response));
     Surge::RtspResponse response(&resp);
 
     ASSERT_STREQ(response.GetBodyString().c_str(), "BODY\r\nBODY\r\n");
 }
+
+TEST(RTSP_RESPONSE, TestThatCanParseBodyAndReturnCorrectLength) {
+    const char *test_response =
+        "RTSP/1.0 200 OK\r\n"
+        "CSeq: 0\r\n"
+        "Content-Type: application/sdp\r\n"
+        "\r\n"
+        "BODY\r\n"
+        "BODY\r\n"
+        "\r\n";
+
+    Surge::Response resp((unsigned char *)test_response, strlen(test_response));
+    Surge::RtspResponse response(&resp);
+
+    ASSERT_EQ(response.GetBodyLength(), 12);
+}
+
+TEST(RTSP_RESPONSE, TestThatFindHeaderValueForKeyReturnsTheValueOnly) {
+    const char *test_response =
+        "RTSP/1.0 200 OK\r\n"
+        "CSeq: 0\r\n"
+        "Content-Type: application/sdp\r\n"
+        "\r\n"
+        "BODY\r\n"
+        "BODY\r\n"
+        "\r\n";
+
+    Surge::Response resp((unsigned char *)test_response, strlen(test_response));
+    Surge::RtspResponse response(&resp);
+
+    ASSERT_STREQ(response.HeaderValueForKey("CSeq").c_str(), "0");
+}
+
+TEST(RTSP_RESPONSE, TestThatFindHeaderValueForKeyReturnsEmptyWhenMissing) {
+    const char *test_response =
+        "RTSP/1.0 200 OK\r\n"
+        "CSeq: 0\r\n"
+        "Content-Type: application/sdp\r\n"
+        "\r\n"
+        "BODY\r\n"
+        "BODY\r\n"
+        "\r\n";
+
+    Surge::Response resp((unsigned char *)test_response, strlen(test_response));
+    Surge::RtspResponse response(&resp);
+
+    ASSERT_STREQ(response.HeaderValueForKey("invalid_header").c_str(), "");
+}
+

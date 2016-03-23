528f11530d400bc5c9eb46e4a2f8610c2de9a65b
Chris van Es <cvanes@gmail.com>
2016-03-23 23:45:33 +0000

    Adding basic XCode project structure for creating iOS, watchOS and tvOS frameworks

---
+++ src/wrappers/objc-swift/Surge/SurgeTvOS/SurgeTvOS.h
@@ -0,0 +1,19 @@
+//
+//  SurgeTvOS.h
+//  SurgeTvOS
+//
+//  Created by Chris van Es on 23/03/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SurgeTvOS.
+FOUNDATION_EXPORT double SurgeTvOSVersionNumber;
+
+//! Project version string for SurgeTvOS.
+FOUNDATION_EXPORT const unsigned char SurgeTvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurgeTvOS/PublicHeader.h>
+
+
---
+++ src/wrappers/objc-swift/Surge/SurgeiOS/SurgeiOS.h
@@ -0,0 +1,19 @@
+//
+//  SurgeiOS.h
+//  SurgeiOS
+//
+//  Created by Chris van Es on 23/03/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SurgeiOS.
+FOUNDATION_EXPORT double SurgeiOSVersionNumber;
+
+//! Project version string for SurgeiOS.
+FOUNDATION_EXPORT const unsigned char SurgeiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurgeiOS/PublicHeader.h>
+
+

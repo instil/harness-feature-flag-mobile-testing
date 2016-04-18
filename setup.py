16e34efeae12c3eb6f95ee2b3190480f779c164b
Philip Herron <herron.philip@googlemail.com>
2016-04-18 11:14:12 +0100

    Fix python bindings build

--- setup.py
+++ setup.py
@@ -5,27 +5,45 @@ from Cython.Build import cythonize
 from distutils.extension import Extension
 
 extensions = [Extension("Surge",
-                        language="c++",
-                        extra_compile_args=['--std=c++11'],
-                        define_macros=[('CYTHON', '1'),
-                                       ('IS_BIG_ENDIAN', '0')],
-                        sources=['src/wrappers/python/Surge.pyx',
-                                 'src/Base64.cc',
-                                 'src/DateTime.cc',
-                                 'src/ErrorDispatcher.cc',
-                                 'src/FireableEvent.cc',
-                                 'src/H264Depacetizer.cc',               
-                                 'src/Helpers.cc',
-                                 'src/Mutex.cc',
-                                 'src/RtpPacket.cc',
-                                 'src/RtspClient.cc',
-                                 'src/SessionDescriptionFactory.cc',
-                                 'src/SocketHandler.cc',
-                                 'src/Surge.cc',
-                                 'src/Thread.cc',
-                                 'src/WaitableEvents.cc']
+                        language = "c++",
+                        extra_compile_args = ['--std=c++11'],
+                        
+                        define_macros = [('CYTHON', '1'),
+                                         ('BINDING_BUILD', '1'),
+                                         ('IS_BIG_ENDIAN', '0')],
+
+                        include_dirs = ['./RxCpp/Rx/v2/src/'],
+                        
+                        sources = [
+                            "./src/Base64.cc",
+                            "./src/DateTime.cc",
+                            "./src/ErrorDispatcher.cc",
+                            "./src/FireableEvent.cc",
+                            "./src/H264Depacetizer.cc",
+                            "./src/Helpers.cc",
+                            "./src/InterleavedRtspTransport.cc",
+                            "./src/MJPEGDepacketizer.cc",
+                            "./src/MP4VDepacketizer.cc",
+                            "./src/Mutex.cc",
+                            "./src/RtpPacket.cc",
+                            "./src/RtspClient.cc",
+                            "./src/RtspCommandFactory.cc",
+                            "./src/SessionDescriptionFactory.cc",
+                            "./src/SessionDescriptionV0.cc",
+                            "./src/Surge.cc",
+                            "./src/Thread.cc",
+                            "./src/UdpTransport.cc",
+                            "./src/WaitableEvents.cc",
+                            
+                            "./src/wrappers/python/Surge.pyx",
+                        ]
                     )]
 
 setup(
-    ext_modules = cythonize(extensions)
+    name='Surge',
+    version='1.0',
+    description='RTSP implementation supporting TCP-interleaved and UDP',
+    author='Instil Software',
+    url='https://github.com/instil/surge',
+    ext_modules=cythonize(extensions)
 )
--- src/wrappers/python/Surge.pxd
+++ src/wrappers/python/Surge.pxd
@@ -4,12 +4,12 @@ from libcpp.vector cimport vector
 
 
 
-cdef extern from "RtspClient.h" namespace "Surge":
-    cppclass RtspClient:
-        RtspClient()
-        DescribeResponse * Describe(const string url, bool requires_auth, const string user, const string password)
-        SetupResponse * Setup(const SessionDescription sessionDescription, bool serverAllowsAggregate)
-        RtspResponse * Play(bool waitForResponse)
-        RtspResponse * Pause()
-        void StopClient()
+#cdef extern from "RtspClient.h" namespace "Surge":
+#    cppclass RtspClient:
+#        RtspClient()
+#        DescribeResponse * Describe(const string url, bool requires_auth, const string user, const string password)
+#        SetupResponse * Setup(const SessionDescription sessionDescription, bool serverAllowsAggregate)
+#        RtspResponse * Play(bool waitForResponse)
+#        RtspResponse * Pause()
+#        void StopClient()
 

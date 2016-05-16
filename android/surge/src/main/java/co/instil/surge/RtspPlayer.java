aed48fbe738100b9304ae43927bad439faad6dd5
Chris van Es <cvanes@gmail.com>
2016-05-16 18:54:46 +0100

    Adding basic project structure

---
+++ android/surge/src/main/java/co/instil/surge/RtspPlayer.java
@@ -0,0 +1,28 @@
+package co.instil.surge;
+
+import java.net.URL;
+
+/**
+ *
+ */
+public class RtspPlayer {
+
+    private URL url;
+
+    public void initiatePlaybackOf(URL url) {
+
+    }
+
+    public void play() {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void stop() {
+
+    }
+
+}

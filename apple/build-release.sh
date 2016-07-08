33d3b597f22d67d1d66a7dd15a377c66fc849da4
Chris van Es <cvanes@gmail.com>
2016-07-08 21:16:40 +0100

    Updating code signing identity used by jenkins build script

--- apple/build-release.sh
+++ apple/build-release.sh
@@ -9,8 +9,10 @@ cmake -DFOR_IOS=ON -G Xcode ../../core
 cd ..
 echo "Building iOS framework"
 xcodebuild -workspace Surge.xcworkspace -scheme SurgeiOS -configuration Release clean build \
-  TARGET_BUILD_DIR=$(pwd)/build/ios-framework
+  TARGET_BUILD_DIR=$(pwd)/build/ios-framework \
+  CODE_SIGN_IDENTITY="iPhone Developer: Instil Software"
 
 echo "Building macOS framework"
 xcodebuild -workspace Surge.xcworkspace -scheme SurgeMacOS -configuration Release clean build \
-  TARGET_BUILD_DIR=$(pwd)/build/macos-framework
+  TARGET_BUILD_DIR=$(pwd)/build/ios-framework \
+  CODE_SIGN_IDENTITY="iPhone Developer: Instil Software"

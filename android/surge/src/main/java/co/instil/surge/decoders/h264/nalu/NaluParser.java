44742c9ca9f7004ac0ffa780e028392d3acd82f1
Matt McComb <matt.mccomb@instil.co>
2016-07-09 09:45:36 +0100

    First pass at refactoring the NaluParser to improve readability.

--- android/surge/src/main/java/co/instil/surge/decoders/h264/nalu/NaluParser.java
+++ android/surge/src/main/java/co/instil/surge/decoders/h264/nalu/NaluParser.java
@@ -4,7 +4,13 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Parser which extracts {@link NaluSegment} from byte buffer arrays.
+ */
 public class NaluParser {
+
+    private static final int MINIMUM_NAL_UNIT_LENGTH = 6;
+
     /**
      * H264 is comprised of NALU segments.
      *
@@ -20,58 +26,63 @@ public class NaluParser {
      * read until the next magic-byte-sequence AKA the next segment to figure
      * out the full nalu length
      **/
-    public static List<NaluSegment> parseNaluSegments(ByteBuffer byteBuffer) throws Exception {
+    public List<NaluSegment> parseNaluSegments(ByteBuffer byteBuffer) throws Exception {
         byte[] buffer = new byte[byteBuffer.capacity()];
         byteBuffer.get(buffer);
 
         List<NaluSegment> segmentList = new ArrayList<>();
-        if (buffer.length < 6) {
+        NaluSegment previousSegment = null;
+
+        if (buffer.length < MINIMUM_NAL_UNIT_LENGTH) {
             return segmentList;
         }
 
-        int lastStartingOffset = -1;
         for (int i = 0; i < buffer.length - 10; ++i) {
-            if (buffer[i] == 0x00 && buffer[i+1] == 0x00 && buffer[i+2] == 0x01) {
-                int naluType = (buffer[i+3] & 0x1F);
-                NaluSegment segment = new NaluSegment(naluType, 3, i);
 
-                if (i > 0 && buffer[i-1] == 0x00) {
-                    // This is actually a 4 byte segment
-                    int currentSegmentOffset = segment.getOffset();
-                    segment.setHeaderSize(4);
-                    segment.setOffset(currentSegmentOffset - 1);
+            int magicByteLength = lengthOfMagicByteSequenceAtPosition(buffer, i);
+            boolean isStartOfNalUnit = magicByteLength != -1;
+            if (isStartOfNalUnit) {
+                int naluType = (buffer[i+magicByteLength] & 0x1F);
+                NaluSegment segment = new NaluSegment(naluType, magicByteLength, i);
+                segment.setHeaderSize(magicByteLength);
+                if (previousSegment != null) {
+                    copyBufferIntoSegment(buffer, previousSegment.getOffset(), i - previousSegment.getOffset(), previousSegment);
                 }
-
-                if (lastStartingOffset > -1) {
-                    NaluSegment lastSegment = segmentList.get(segmentList.size() - 1);
-
-                    int bufferLength = segment.getOffset() - lastSegment.getOffset();
-                    byte[] segmentBuffer = new byte[bufferLength];
-
-                    System.arraycopy(buffer, lastSegment.getOffset(), segmentBuffer, 0, bufferLength);
-                    lastSegment.setBuffer(segmentBuffer);
-
-                    segmentList.remove(segmentList.size() - 1);
-                    segmentList.add(lastSegment);
-                }
-
                 segmentList.add(segment);
-                lastStartingOffset = segment.getOffset();
+                previousSegment = segment;
             }
         }
 
-        if (segmentList.size() > 0) {
-            NaluSegment lastSegment = segmentList.get(segmentList.size() - 1);
-
-            int bufferLength = buffer.length - lastSegment.getOffset();
-            byte[] segmentBuffer = new byte[bufferLength];
-            System.arraycopy(buffer, lastSegment.getOffset(), segmentBuffer, 0, bufferLength);
-            lastSegment.setBuffer(segmentBuffer);
-
-            segmentList.remove(segmentList.size() - 1);
-            segmentList.add(lastSegment);
+        if (previousSegment != null) {
+            copyBufferIntoSegment(buffer, previousSegment.getOffset(), buffer.length - previousSegment.getOffset(), previousSegment);
         }
         return segmentList;
+    }
+
+    /**
+     * Determines if the next sequence of bytes in the supplied buffer at the specified position
+     * represents a magic byte sequence (start of a NAL unit).  The magic byte sequence can be
+     * 3 or 4 bytes long (e.g. 0x000001 or 0x00000001).
+     * @param buffer byte buffer containing a sequence of NAL units.
+     * @param position the position at which the magic byte sequence may start.
+     * @return the length of the magic byte sequence at the position (3 or 4); or -1 a magic
+     * byte sequence does not start at the supplied position.
+     */
+    private int lengthOfMagicByteSequenceAtPosition(byte[] buffer, int position) {
+        boolean startsWithZeroBytes = buffer[position] == 0x00 && buffer[position+1] == 0x00;
+        if (startsWithZeroBytes && buffer[position+2] == 0x01) {
+            return 3;
+        } else if (startsWithZeroBytes && buffer[position+2] == 0x00 && buffer[position+3] == 0x01) {
+            return 4;
+        } else {
+            return -1;
+        }
+    }
+
+    private void copyBufferIntoSegment(byte[] buffer, int offset, int length, NaluSegment segment) throws Exception {
+        byte[] segmentBuffer = new byte[length];
+        System.arraycopy(buffer, offset, segmentBuffer, 0, length);
+        segment.setBuffer(segmentBuffer);
     }
 
 }

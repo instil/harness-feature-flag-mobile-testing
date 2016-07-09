ecfcf057055e59d2193c7fed0143201c91677a4b
Chris van Es <cvanes@gmail.com>
2016-07-09 17:22:24 +0100

    Adding architectures for simulator and swift modules to iOS framework

--- apple/build-release.sh
+++ apple/build-release.sh
@@ -12,15 +12,29 @@ xctool -workspace Surge.xcworkspace -scheme SurgeCore clean test \
   -reporter pretty \
   -reporter junit:build/test-results/core-lib-tests.xml
 
-echo "Building iOS framework"
-xctool -workspace Surge.xcworkspace -scheme SurgeiOS -configuration Release clean build \
-  CONFIGURATION_BUILD_DIR=$(pwd)/build/frameworks/iOS/release \
-  CODE_SIGN_IDENTITY="iPhone Developer: Instil Software"
-
 echo "Building macOS framework"
 xctool -workspace Surge.xcworkspace -scheme SurgeMacOS -configuration Release clean build \
-  CONFIGURATION_BUILD_DIR=$(pwd)/build/frameworks/macOS/release \
+  CONFIGURATION_BUILD_DIR=$(pwd)/build/frameworks/macOS \
   CODE_SIGN_IDENTITY="iPhone Developer: Instil Software"
 
-cd build/frameworks
-zip -r apple-frameworks.zip .
+echo "Building iOS framework"
+xctool -workspace Surge.xcworkspace -scheme SurgeiOS -configuration Release -sdk iphoneos clean build \
+  CONFIGURATION_BUILD_DIR=$(pwd)/build/frameworks/iOS \
+  CODE_SIGN_IDENTITY="iPhone Developer: Instil Software"
+
+xctool -workspace Surge.xcworkspace -scheme SurgeiOS -configuration Release -sdk iphonesimulator clean build \
+  CONFIGURATION_BUILD_DIR=$(pwd)/build/frameworks/iOS/simulator \
+  CODE_SIGN_IDENTITY="iPhone Developer: Instil Software"
+
+cd build/frameworks/iOS
+
+if [ -d "simulator/SurgeiOS.framework/Modules/SurgeiOS.swiftmodule" ]; then
+  cp -r simulator/SurgeiOS.framework/Modules/SurgeiOS.swiftmodule/* SurgeiOS.framework/Modules/SurgeiOS.swiftmodule/
+fi
+
+lipo -create -output SurgeiOS.framework/SurgeiOS SurgeiOS.framework/SurgeiOS simulator/SurgeiOS.framework/SurgeiOS
+
+rm -rf simulator
+
+cd ..
+zip -r apple-frameworks.zip iOS/SurgeiOS.framework/ macOS/SurgeMacOS.framework/

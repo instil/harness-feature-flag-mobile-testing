d0359e9dc0ec2d2fae7895bc5372a735e68ca6ea
paulshields <paul.shields@instil.co>
2016-07-19 11:26:58 +0100

    Updated the Xamarin build scripts to build dll files for each bindings project

--- xamarin/build-android.sh
+++ xamarin/build-android.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BUILD_DIR=$(pwd)/SurgeXamarinAndroid/SurgeXamarinAndroidBindings/Jars
+XAMARIN_DIR=$(pwd)
+BUILD_DIR=$XAMARIN_DIR/SurgeXamarinAndroid/SurgeXamarinAndroidBindings/Jars
 SURGE_DIR=../android
 
 rm $BUILD_DIR/SurgeAndroid.aar
@@ -12,3 +13,5 @@ cd $SURGE_DIR
 ./gradlew cleanLibs
 
 cp $SURGE_DIR/surge/build/outputs/aar/surge-release.aar $BUILD_DIR/SurgeAndroid.aar
+
+xbuild /p:Configuration=Release $XAMARIN_DIR/SurgeXamarinAndroid/SurgeXamarinAndroid.sln
--- xamarin/build-ios.sh
+++ xamarin/build-ios.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+XAMARIN_DIR=$(pwd)
 SURGE_DIR=../apple
-BUILD_DIR=$(pwd)/SurgeXamariniOS/lib
+BUILD_DIR=$XAMARIN_DIR/SurgeXamariniOS/lib
 
-rm -r $BUILD_DIR/SurgeiOS*.framework
-rm $BUILD_DIR/libsurge.a
+rm -r $BUILD_DIR
+mkdir $BUILD_DIR
 
 cd $SURGE_DIR
 
 $SURGE_DIR/build-release.sh
-cp -r $SURGE_DIR/build/frameworks/SurgeiOS.framework $BUILD_DIR/
-cp -r $SURGE_DIR/build/frameworks/libsurge.a $BUILD_DIR/
+cp -r $SURGE_DIR/build/frameworks/iOS/SurgeiOS.framework $BUILD_DIR/
+cp -r $SURGE_DIR/build/frameworks/iOS/libSurgeCore.a $BUILD_DIR/
+
+"/Applications/Xamarin Studio.app/Contents/MacOS/mdtool" -v build "--configuration:Release" $XAMARIN_DIR/SurgeXamariniOS/SurgeXamariniOS.sln

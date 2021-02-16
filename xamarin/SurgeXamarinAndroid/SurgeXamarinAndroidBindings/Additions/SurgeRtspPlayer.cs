6c0bda239e5926bed3a7719ab017b705b6e5e39b
Paul Shields <paul.shields@instil.co>
2021-02-16 12:45:37 +0000

    Merged in no-ticket/xamarin-seek-action-callback (pull request #56)
    C# seek callback API improvement
    
    * Adding an extension method to avoid needing to create manual instances of Java callback classes. Upgrading Xamarin Android dependencies.
    
    Approved-by: Niall Robson
    Approved-by: Niall Kelly

--- xamarin/SurgeXamarinAndroid/SurgeXamarinAndroidBindings/Additions/SurgeRtspPlayer.cs
+++ xamarin/SurgeXamarinAndroid/SurgeXamarinAndroidBindings/Additions/SurgeRtspPlayer.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using Android.Runtime;
-using Android.Views;
 using Java.Util;
 using CO.Instil.Surge.Client;
 
@@ -10,20 +7,21 @@ namespace CO.Instil.Surge.Player {
 
         public PlayerCallback playerCallback;
 
-        public void InitiatePlaybackOf(string url, Action<RtspErrorCode> callback)
-        {
+        public void InitiatePlaybackOf(string url, Action<RtspErrorCode> callback) {
             InitiatePlaybackOf(url, "", "", null, null, callback);
         }
 
-        public void InitiatePlaybackOf(string url, string username, string password, Action<RtspErrorCode> callback)
-        {
+        public void InitiatePlaybackOf(string url, string username, string password, Action<RtspErrorCode> callback) {
             InitiatePlaybackOf(url, username, password, null, null, callback);
         }
 
-        public void InitiatePlaybackOf(string url, string username, string password, Date startTime, Date endTime, Action<RtspErrorCode> callback)
-        {
+        public void InitiatePlaybackOf(string url, string username, string password, Date startTime, Date endTime, Action<RtspErrorCode> callback) {
             playerCallback = new PlayerCallback(callback);
             InitiatePlaybackOf(url, username, password, startTime, endTime, playerCallback);
+        }
+
+        public void Seek(Date startTime, Date endTime, Action<RtspErrorCode> callback) {
+            Seek(startTime, endTime, new PlayerCallback(callback));
         }
     }
 }

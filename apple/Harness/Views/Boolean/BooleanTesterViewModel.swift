a246c367f2c74ad2a6102df2bf6146ecca9c65f2
Paul Shields <paul.shields@instil.co>
2022-09-27 14:02:02 +0100

    Adding feature flags to all 5 text views on the basic boolean view. (#3)

--- apple/Harness/Views/Boolean/BooleanTesterViewModel.swift
+++ apple/Harness/Views/Boolean/BooleanTesterViewModel.swift
@@ -7,14 +7,33 @@ import ff_ios_client_sdk
 
 class BooleanTesterViewModel: ObservableObject {
     @Published var booleanOne: Bool = true
+    @Published var booleanTwo: Bool = true
+    @Published var booleanThree: Bool = true
+    @Published var booleanFour: Bool = true
+    @Published var booleanFive: Bool = true
 
     let featureFlagService = FeatureFlagService()
     
     init() {
         featureFlagService.load() { [weak self] in
             self?.featureFlagService.boolVariation(evaluationId: "Boolean1") { (response) in
-                self?.booleanOne = response?.identifier == "true"
-                print(String(self?.booleanOne ?? false))
+                self?.booleanOne = response
+            }
+            
+            self?.featureFlagService.boolVariation(evaluationId: "Boolean2") { (response) in
+                self?.booleanTwo = response
+            }
+            
+            self?.featureFlagService.boolVariation(evaluationId: "Boolean3") { (response) in
+                self?.booleanThree = response
+            }
+            
+            self?.featureFlagService.boolVariation(evaluationId: "Boolean4") { (response) in
+                self?.booleanFour = response
+            }
+            
+            self?.featureFlagService.boolVariation(evaluationId: "Boolean5") { (response) in
+                self?.booleanFive = response
             }
         }
     }

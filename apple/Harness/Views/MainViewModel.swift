6cebbbcf242b615e1ee6d249a8b31400c34edb3e
Paul Shields <paul.shields@instil.co>
2022-10-04 15:25:32 +0100

    Adding buttons to close and suspend the app to the main view. (#10)

--- apple/Harness/Views/MainViewModel.swift
+++ apple/Harness/Views/MainViewModel.swift
@@ -3,6 +3,7 @@
 //
 
 import Foundation
+import UIKit
 
 class MainViewModel: ObservableObject {
     @Published var useRealService = true {
@@ -11,5 +12,14 @@ class MainViewModel: ObservableObject {
     
     @Inject private var featureFlagService: SwappableFeatureFlagService {
         didSet { self.useRealService = self.featureFlagService.useRealService }
+    }
+    
+    func suspendApp() {
+        UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+    }
+    
+    func closeApp() {
+        // NOTE: Including this in an app will cause it to be rejected from the app store
+        exit(0)
     }
 }

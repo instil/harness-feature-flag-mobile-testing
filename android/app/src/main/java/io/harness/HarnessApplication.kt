4f8813299456f5cbe039132070e0ec5f6c2ca411
Gareth Fleming <gareth.fleming@instil.co>
2022-11-22 15:01:21 +0000

    Moved event listener from HarnessApplication to HarnessFeatureFlagService.

--- android/app/src/main/java/io/harness/HarnessApplication.kt
+++ android/app/src/main/java/io/harness/HarnessApplication.kt
@@ -1,16 +1,10 @@
 package io.harness
 
 import android.app.Application
-import android.content.Intent
 import android.util.Log
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import dagger.hilt.android.HiltAndroidApp
-import io.harness.cfsdk.CfClient
-import io.harness.cfsdk.cloud.core.model.Evaluation
-import io.harness.cfsdk.cloud.oksse.model.StatusEvent
 import io.harness.services.FeatureFlagService
 import io.harness.settings.SettingsRepository
-import io.harness.settings.SettingsRepository.Companion.SETTING_REFRESH_UI
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -30,30 +24,6 @@ class HarnessApplication : Application() {
         MainScope().launch {
             featureFlagService.load {
                 Log.d("HARNESS APP", "Feature flag service has finished loading")
-                CfClient.getInstance().registerEventsListener { result ->
-                    when (result.eventType) {
-                        StatusEvent.EVENT_TYPE.SSE_START -> Log.d("HARNESS APP", "SSE Stream has been opened")
-                        StatusEvent.EVENT_TYPE.SSE_END -> Log.d("HARNESS APP", "SSE Stream has completed")
-                        StatusEvent.EVENT_TYPE.EVALUATION_CHANGE -> {
-                            val evaluation = result.extractPayload<Evaluation>()
-                            Log.d("HARNESS APP", "Evaluation has changed: ${evaluation.flag} is now ${evaluation.value}")
-                            if (settingsRepository.get(SETTING_REFRESH_UI, "false").toBoolean()) {
-                                broadcastEvaluationChange(evaluation)
-                            }
-                        }
-                        StatusEvent.EVENT_TYPE.EVALUATION_REMOVE -> {
-                            val evaluation = result.extractPayload<Evaluation>()
-                            Log.d("HARNESS APP", "Evaluation has been removed: ${evaluation.flag}")
-                        }
-                        StatusEvent.EVENT_TYPE.EVALUATION_RELOAD -> {
-                            val evaluations = result.extractPayload<List<Evaluation>>()
-                            evaluations.forEach {
-                                Log.d("HARNESS APP", "Evaluation has been reloaded: ${it.flag} is now ${it.value}")
-                            }
-                        }
-                        null -> Log.d("HARNESS APP", "Event listener received null event")
-                    }
-                }
             }
         }
     }
@@ -63,35 +33,7 @@ class HarnessApplication : Application() {
         featureFlagService.destroy()
     }
 
-    private fun broadcastEvaluationChange(ev: Evaluation) {
-        Log.d(TAG, "Broadcasting evaluation update for ${ev.flag}")
-        Intent().also { intent ->
-            intent.putExtra(EXTRA_EVALUATION_FLAG, ev.flag)
-            when (ev.kind) {
-                "boolean" -> {
-                    intent.action = ACTION_BOOLEAN_UPDATE
-                    intent.putExtra(EXTRA_EVALUATION_VALUE, (ev.value as String).toBoolean())
-                }
-                "string" -> {
-                    intent.action = ACTION_STRING_UPDATE
-                    intent.putExtra(EXTRA_EVALUATION_VALUE, ev.value as String)
-                }
-                "int" -> {
-                    intent.action = ACTION_INT_UPDATE
-                    try {
-                        intent.putExtra(EXTRA_EVALUATION_VALUE, (ev.value as String).toInt())
-                    } catch (e: java.lang.NumberFormatException) {
-                        intent.putExtra(EXTRA_EVALUATION_VALUE, -1)
-                    }
-                }
-                "json" -> {
-                    intent.action = ACTION_JSON_UPDATE
-                    intent.putExtra(EXTRA_EVALUATION_VALUE, ev.value as String)
-                }
-            }
-            LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
-        }
-    }
+
 
     companion object {
         const val TAG = "HarnessApplication"
--- android/app/src/main/java/io/harness/services/HarnessFeatureFlagService.kt
+++ android/app/src/main/java/io/harness/services/HarnessFeatureFlagService.kt
@@ -1,11 +1,17 @@
 package io.harness.services
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import dagger.hilt.android.qualifiers.ApplicationContext
+import io.harness.HarnessApplication
 import io.harness.cfsdk.CfClient
 import io.harness.cfsdk.CfConfiguration
+import io.harness.cfsdk.cloud.core.model.Evaluation
 import io.harness.cfsdk.cloud.model.Target
+import io.harness.cfsdk.cloud.oksse.EventsListener
+import io.harness.cfsdk.cloud.oksse.model.StatusEvent
 import io.harness.settings.SettingsRepository
 import io.harness.settings.SettingsRepository.Companion.SETTING_SDK_KEY
 import io.harness.settings.SettingsRepository.Companion.SETTING_TARGET_ID
@@ -37,10 +43,13 @@ class HarnessFeatureFlagService @Inject constructor(
             target
         )
 
+        registerEventListener()
+
         callback()
     }
 
     override fun destroy() {
+        unregisterEventListener()
         CfClient.getInstance().destroy()
     }
 
@@ -62,5 +71,68 @@ class HarnessFeatureFlagService @Inject constructor(
     override fun jsonVariation(evaluationId: String, callback: FeatureFlagCallback<JSONString>) {
         val evaluation = CfClient.getInstance().jsonVariation(evaluationId, JSONObject("{}"))
         callback(evaluation.toString())
+    }
+
+    private val eventsListener = EventsListener { statusEvent ->
+        when (statusEvent.eventType) {
+            StatusEvent.EVENT_TYPE.SSE_START -> Log.d("HARNESS APP", "SSE Stream has been opened")
+            StatusEvent.EVENT_TYPE.SSE_END -> Log.d("HARNESS APP", "SSE Stream has completed")
+            StatusEvent.EVENT_TYPE.EVALUATION_CHANGE -> {
+                val evaluation = statusEvent.extractPayload<Evaluation>()
+                Log.d("HARNESS APP", "Evaluation has changed: ${evaluation.flag} is now ${evaluation.value}")
+                if (settingsRepository.get(SettingsRepository.SETTING_REFRESH_UI, "false").toBoolean()) {
+                    broadcastEvaluationChange(evaluation)
+                }
+            }
+            StatusEvent.EVENT_TYPE.EVALUATION_REMOVE -> {
+                val evaluation = statusEvent.extractPayload<Evaluation>()
+                Log.d("HARNESS APP", "Evaluation has been removed: ${evaluation.flag}")
+            }
+            StatusEvent.EVENT_TYPE.EVALUATION_RELOAD -> {
+                val evaluations = statusEvent.extractPayload<List<Evaluation>>()
+                evaluations.forEach {
+                    Log.d("HARNESS APP", "Evaluation has been reloaded: ${it.flag} is now ${it.value}")
+                }
+            }
+            null -> Log.d("HARNESS APP", "Event listener received null event")
+        }
+    }
+
+    private fun registerEventListener() {
+        CfClient.getInstance().registerEventsListener(eventsListener)
+    }
+
+    private fun unregisterEventListener() {
+        CfClient.getInstance().unregisterEventsListener(eventsListener)
+    }
+
+    private fun broadcastEvaluationChange(ev: Evaluation) {
+        Log.d(HarnessApplication.TAG, "Broadcasting evaluation update for ${ev.flag}")
+        Intent().also { intent ->
+            intent.putExtra(HarnessApplication.EXTRA_EVALUATION_FLAG, ev.flag)
+            when (ev.kind) {
+                "boolean" -> {
+                    intent.action = HarnessApplication.ACTION_BOOLEAN_UPDATE
+                    intent.putExtra(HarnessApplication.EXTRA_EVALUATION_VALUE, (ev.value as String).toBoolean())
+                }
+                "string" -> {
+                    intent.action = HarnessApplication.ACTION_STRING_UPDATE
+                    intent.putExtra(HarnessApplication.EXTRA_EVALUATION_VALUE, ev.value as String)
+                }
+                "int" -> {
+                    intent.action = HarnessApplication.ACTION_INT_UPDATE
+                    try {
+                        intent.putExtra(HarnessApplication.EXTRA_EVALUATION_VALUE, (ev.value as String).toInt())
+                    } catch (e: java.lang.NumberFormatException) {
+                        intent.putExtra(HarnessApplication.EXTRA_EVALUATION_VALUE, -1)
+                    }
+                }
+                "json" -> {
+                    intent.action = HarnessApplication.ACTION_JSON_UPDATE
+                    intent.putExtra(HarnessApplication.EXTRA_EVALUATION_VALUE, ev.value as String)
+                }
+            }
+            LocalBroadcastManager.getInstance(context).sendBroadcast(intent)
+        }
     }
 }
